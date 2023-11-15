61953e1c2d8e6d4794dedb00f6f09f0f7b6d3b46
Luiz Silva <lalsilva@gmail.com>
2023-11-15 14:15:21 -0300

    Filtro com o comando IS

--- insert.sql
+++ insert.sql
@@ -11,12 +11,12 @@ INSERT INTO aluno (
 	ativo
 ) VALUES (
 	gen_random_uuid (),
-	'Luis Silva',
-	'49544742050',
+	'Elisabete Silva',
+	null,
 	'1985-11-20',
 	' Copo furadis é disculpa de bebadis, arcu quam euismod magna. Praesent malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate dapibus. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.',
 	NOW(),
-	6,
+	7,
 	TRUE
 );
 
