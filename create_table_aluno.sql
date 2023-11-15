28a86c645604ce423a200b85baeece1b4492bc1e
Luiz Silva <lalsilva@gmail.com>
2023-11-15 13:45:35 -0300

    Script de criação de tabela

---
+++ create_table_aluno.sql
@@ -0,0 +1,14 @@
+SELECT gen_random_uuid ();
+
+CREATE TABLE aluno(
+	id UUID,
+	nome VARCHAR(255),
+	cpf CHAR(11),
+	data_nascimento DATE,
+	observacao TEXT,
+	data_matricula TIMESTAMP,
+	matricula INTEGER,
+	ativo BOOLEAN
+);
+
+SELECT * FROM aluno;
