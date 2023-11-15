7bc774c7c114fd5032f6a7734f4909979ee4888d
Luiz Silva <lalsilva@gmail.com>
2023-11-15 15:35:41 -0300

    Definindo alias para colunas de mesmo nome e trazendo apenas os nomes do aluno e do curso

--- join.sql
+++ join.sql
@@ -1,4 +1,5 @@
-SELECT *
+SELECT aluno.nome AS "Aluno",
+       curso.nome AS "Curso"
   FROM aluno
   JOIN aluno_curso ON aluno_curso.aluno_id = aluno.id
   JOIN curso ON aluno_curso.curso_id = curso.id;
