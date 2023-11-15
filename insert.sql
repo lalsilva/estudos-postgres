a2301c5c947df0f6f1609a6c05ac36fb1a6147be
Luiz Silva <lalsilva@gmail.com>
2023-11-15 16:32:54 -0300

    Incluindo registro a tabela de aluno

--- insert.sql
+++ insert.sql
@@ -11,7 +11,7 @@ INSERT INTO aluno (
 	ativo
 ) VALUES (
 	gen_random_uuid (),
-	'Elisabete Silva',
+	'João Moreira',
 	null,
 	'1985-11-20',
 	' Copo furadis é disculpa de bebadis, arcu quam euismod magna. Praesent malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate dapibus. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.',
