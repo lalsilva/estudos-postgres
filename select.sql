8921670c24829279770935e15e4f413b449211f0
Luiz Silva <lalsilva@gmail.com>
2023-11-15 16:26:12 -0300

    Ordenação de SELECT pelo ORDER BY

--- select.sql
+++ select.sql
@@ -32,3 +32,12 @@ SELECT *
        OR nome LIKE 'S%'
 	 )
      AND matricula BETWEEN 2 AND 5;
+
+SELECT * FROM aluno ORDER BY nome DESC;
+
+SELECT aluno.nome AS "Aluno",
+       curso.nome AS "Curso"
+  FROM aluno
+  JOIN aluno_curso ON aluno_curso.aluno_id = aluno.id
+  JOIN curso ON aluno_curso.curso_id = curso.id
+  ORDER BY aluno.nome;
