08212c3683f760aab7cedacef8b97261f72ef1fd
Luiz Silva <lalsilva@gmail.com>
2023-11-15 14:10:18 -0300

    LIKE com operador especial % (porcentagem)

--- select.sql
+++ select.sql
@@ -8,7 +8,11 @@ SELECT
 	data_nascimento AS "Data de Nascimento"
 FROM
 	aluno;
-	
+
+/* o % é um "coringa" no filtro LIKE e pode ser usado:
+'% => no início para filtrar qualquer registro que tenha a expressão após ele;
+'TEXTO&' => no final para filtrar qualquer registro que tenha a expressão antes dele e
+'%TEXTO&' => no meio para filtrar qualquer registro que tenha a expressão entre eles */
 SELECT *
 	FROM aluno
-   WHERE nome LIKE 'L_is%';
+   WHERE nome LIKE '%Antônio';
