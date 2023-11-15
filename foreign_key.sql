4f591b78c139ef6893e2a44cc753cb2fc12f4e31
Luiz Silva <lalsilva@gmail.com>
2023-11-15 16:17:03 -0300

    Incluindo a instrução ON UPDATE CASCADE na chave estrangeira aluno_id

--- foreign_key.sql
+++ foreign_key.sql
@@ -6,11 +6,13 @@ CREATE TABLE aluno_curso(
 	PRIMARY KEY (aluno_id, curso_id),
 	FOREIGN KEY (aluno_id)
 		REFERENCES aluno(id)
-		ON DELETE CASCADE,
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
 	FOREIGN KEY (curso_id) REFERENCES curso(id)
 );
 
 DELETE FROM aluno WHERE id = '46eaca0e-01fb-4d58-abf3-ce12246135f1';
+UPDATE aluno SET id = '1947572e-f174-4945-b0e5-0c5e24ea24ef' WHERE id = '76cd6234-5ccc-45b0-a9a1-297701c86071';
 
 SELECT * FROM aluno_curso;
 SELECT * FROM aluno;
@@ -20,6 +22,6 @@ INSERT INTO aluno_curso (
 	aluno_id,
 	curso_id
 ) VALUES (
-	'46eaca0e-01fb-4d58-abf3-ce12246135f1',
+	'76cd6234-5ccc-45b0-a9a1-297701c86071',
 	1
 );
